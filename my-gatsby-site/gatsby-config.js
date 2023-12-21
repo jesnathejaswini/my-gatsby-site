9ffa6b57f7158176cf9d7dec78fec22bbd1ced21
jesnathejaswini <jesnajohnclt@gmail.com>
2023-12-21 15:13:34 +0530

    trial project added

---
+++ my-gatsby-site/gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
